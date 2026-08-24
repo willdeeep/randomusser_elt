bb895e8b25f7ac2feeec828f507c66bf156d35f2
willdeeep <23581815+willdeeep@users.noreply.github.com>
2026-08-24 12:19:57 +0100

    draft: moved write function to it's own module and set up empty dudner modules for main and init.

---
+++ src/randomuser_elt/write.py
@@ -0,0 +1,8 @@
+"""Write API response text to CSV
+
+Write response text from client to CSV in dbt/seeds directory, raise exceptions as appropriate
+"""
+
+def seed_response_csv(response):
+    with open("dbt/seeds/randomuser.csv", "w") as f:
+        f.write(response.text)
