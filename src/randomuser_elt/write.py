06a359278bcfbd13c2528d9cbfed59a17301d6d7
willdeeep <23581815+willdeeep@users.noreply.github.com>
2026-08-25 15:39:06 +0100

    format: Minor format line change to write.py.

--- src/randomuser_elt/write.py
+++ src/randomuser_elt/write.py
@@ -3,6 +3,7 @@
 Write response text from client to CSV in dbt/seeds directory, raise exceptions as appropriate
 """
 
+
 def seed_response_csv(response):
     with open("dbt/seeds/randomuser.csv", "w") as f:
         f.write(response.text)
